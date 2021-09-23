f3e338bc16812b4634eaa0fe06da282a678345d9
usuario <usuario@LAPTOP-EPUPVD6Q>
2021-09-23 08:31:15 +0200

    update

---
+++ FlowPanel/src/application/Main.java
@@ -0,0 +1,55 @@
+package application;
+	
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		FlowPane raiz = new FlowPane();
+		
+		raiz.setHgap(20);
+		raiz.setVgap(20);
+		
+		
+		Button btnAceptar = new Button("Aceptar");
+		Button btnCancelar = new Button("Cancelar");
+		Button btnAtras = new Button("Atras");
+		Button btnDelante = new Button("Delante");
+		Label lbTexto = new Label("hola esto es una ventana de javafx");
+		
+		
+		raiz.setMargin(btnDelante, new Insets(10, 0, 10, 0));
+		
+		
+		raiz.getChildren().add(btnAceptar);
+		raiz.getChildren().add(btnCancelar);
+		raiz.getChildren().add(btnAtras);
+		raiz.getChildren().add(btnDelante);
+		raiz.getChildren().add(lbTexto);
+		
+	
+		
+		Scene escena = new Scene(raiz, 350, 100);
+		
+		primaryStage.setScene(escena);
+		
+		primaryStage.setTitle("FlowPanel");
+		primaryStage.show();
+	
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
