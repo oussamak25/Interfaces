c5119364f3dcc3fa42a98b04fc743fc10a06991f
usuario <usuario@LAPTOP-EPUPVD6Q>
2021-09-28 09:51:07 +0200

    comentarios

--- FlowPanel/src/application/Main.java
+++ FlowPanel/src/application/Main.java
@@ -1,10 +1,6 @@
 package application;
 	
 
-
-
-
-
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -22,45 +18,52 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		
+		//Creamos la raiz que será un stackPane que hara la función de fondo
 		StackPane raiz = new StackPane();
+		//le damos unos margenes al contenedor
 		raiz.setPadding(new Insets(5));
 		Label lbTexto = new Label("Nueva Ventana de JavaFx");
 		
 				
 		raiz.setAlignment(Pos.TOP_CENTER);
 		
-		
+		//Creamos un contenedor tipo flowpane 
 		FlowPane flowpanel = new FlowPane();
 		
-
+		//definimos los valores de espaciado entre los elementos del flowpane
 		flowpanel.setHgap(20);
 		flowpanel.setVgap(20);
 		
+		//le damos unos margenes al contenedor y la alineación
 		flowpanel.setPadding(new Insets(20));
 		flowpanel.setAlignment(Pos.BOTTOM_CENTER);
 		
+		
+		//creamos unos cuantos botones y les cambiamos el color de fondo 
 		Button btnAceptar = new Button("Aceptar");
 		Button btnCancelar = new Button("Cancelar");
 		Button btnAtras = new Button("Atras");
 		Button btnDelante = new Button("Delante");
 		
 		btnAceptar.setStyle("-fx-background-color: #8280F5;");
-	        btnAtras.setStyle("-fx-background-color: #8280F5;");
-	        btnCancelar.setStyle("-fx-background-color: #8280F5;");
-	        btnDelante.setStyle("-fx-background-color: #8280F5;");
-	        lbTexto.setStyle("-fx-background-color: #FFFFFF;");
-	        lbTexto.setPadding(new Insets(20));
+	    btnAtras.setStyle("-fx-background-color: #8280F5;");
+	    btnCancelar.setStyle("-fx-background-color: #8280F5;");
+	    btnDelante.setStyle("-fx-background-color: #8280F5;");
+	    lbTexto.setStyle("-fx-background-color: #FFFFFF;");
+	    lbTexto.setPadding(new Insets(20));
 
-
+	    //añadimos los botones al contenedor 
 		flowpanel.getChildren().add(btnAceptar);
 		flowpanel.getChildren().add(btnCancelar);
 		flowpanel.getChildren().add(btnAtras);
 		flowpanel.getChildren().add(btnDelante);
 		
 		
-		
+		//añadimos un rectangulo con color gris para el fondo, el cuadro de texto y el contenedor flowpanel
 		raiz.getChildren().addAll(new Rectangle(Integer.MAX_VALUE,Integer.MAX_VALUE, Color.DARKGREY), lbTexto, flowpanel);
 		
+		
+		//se crea una escena
 		Scene escena = new Scene(raiz, 500, 250);
 		
 		primaryStage.setScene(escena);
